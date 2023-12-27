a2b8e46114e99727c984d822a90b8a7bd13af233
turboBasic ⚡️💀 <off@boun.cr>
2023-12-27 11:41:10 +0100

    test: Add test for project's root Jenkinsfile
    Resolves #2

---
+++ test/org/example/scripts/JenkinsfileSpec.groovy
@@ -0,0 +1,20 @@
+package org.example.scripts
+
+import testSupport.PipelineSpockTestBase
+
+
+class JenkinsfileSpec extends PipelineSpockTestBase {
+
+    def 'Jenkinsfile works as expected'() {
+        given:
+            helper.addShMock('./runDevEnvironment.sh', '...execute build environment...', 0)
+        when:
+            runScript 'Jenkinsfile'
+        then:
+            printCallStack()
+            assertJobStatusSuccess()
+        then:
+            testNonRegression 'Jenkinsfile_works_as_expected'
+    }
+
+}
